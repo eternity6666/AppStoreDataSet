72816370a4abfb0553891dc1fc822bf2363f2f65
eternity6666 <eternity6666@qq.com>
2025-03-17 22:26:04 +0800

    delete 2025-02

--- python/deleteFile.py
+++ python/deleteFile.py
@@ -26,7 +26,7 @@ def deleteFile(path, targetPath):
 
 if __name__ == '__main__':
     dictPathList = ['ipad', 'iphone']
-    needRemoveDataList = ['2025-02-21', '2025-02-22']
+    needRemoveDataList = ['2025-02']
     for dictPath in dictPathList:
         for targetPath in needRemoveDataList:
             deleteFile(dictPath, targetPath)
